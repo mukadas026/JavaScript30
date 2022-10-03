2011cabbaa30b26a2ccdaff0afd2bcd41ef5a852
mukadas026 <mukadasm026@gmail.com>
2022-10-03 20:29:11 +0000

    modified project 10

--- 10 - Shift Select/index.js
+++ 10 - Shift Select/index.js
@@ -5,33 +5,65 @@ let clicked = false, indexStart, indexEnd;
 
 checkbox.forEach((value, i) => {
     value.addEventListener('click', (e) => {
-        if(clicked === false || value.checked === true){
+        if(e.shiftKey){
+            indexEnd = i;
+            console.log('hello')
+            return check();
+        }
+        if(value.checked === true){
             clicked = true;
             indexStart = i;
+            para[indexStart].style.textDecoration = 'line-through';
+
             return
         }
-        if(value.checked === false){
-            clicked = false
+        if(value.checked === false ){
+            clicked = false;
+            indexStart = i;
+            para[indexStart].style.textDecoration = 'none';
+            // indexStart = null;
+
             return 
         }
+        
     })
 })
 
-window.addEventListener('keydown', (e) => {
-    if(e.key ==='Shift'){
-        checkbox.forEach((value, i) => {
-            value.addEventListener('click', (evt) => {
-                indexEnd = i;
-                return check()
-            })
-        })        
-    }
-})
+// document.addEventListener('keydown', (e) => {
+    // console.log(e)
+    // if(e.key === 'Shift'){
+        // checkbox.forEach((value, i) => {
+        //     value.addEventListener('click', (e) => {
+        //         if(e.shiftKey){
+        //         indexEnd = i;
+        //         console.log('hello')
+        //         return check();
+        //         }
+        //     })
+        // })        
+//     }
+// })
 
     
 function check(){
-    for(indexStart; indexStart <= indexEnd; indexStart++){
-        checkbox[indexStart].checked = true
-        para[indexStart].style.textDecoration = 'line-through'
+    if(indexStart < indexEnd){
+        for(indexStart; indexStart <= indexEnd; indexStart++){
+            para[indexStart].style.textDecoration = 'line-through';
+            checkbox[indexStart].checked = true;
+            console.log(indexStart, indexEnd)
+            if(indexStart === indexEnd){
+                break;
+            }
+            
+        }
+    }
+    else if(indexStart > indexEnd){
+        for(indexEnd; indexEnd <= indexStart; indexEnd++){
+            checkbox[indexEnd].checked = true;
+            para[indexEnd].style.textDecoration = 'line-through';
+            if(indexEnd === indexStart){
+                break;
+            }
+        }
     }
 }

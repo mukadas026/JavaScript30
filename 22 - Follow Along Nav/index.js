3c71b600d787843b491edb267448f124375620ba
mukadas026 <mukadasm026@gmail.com>
2022-10-20 22:25:04 +0000

    changed some of the js

--- 22 - Follow Along Nav/index.js
+++ 22 - Follow Along Nav/index.js
@@ -114,35 +114,49 @@ const hover =  document.querySelector('#hover')
 const items = document.querySelectorAll('.nav-item')
 
 items.forEach(value => {
-    let styles = getComputedStyle(value)
+    // let styles = getComputedStyle(value)
+
+
+
     value.addEventListener('mouseover', (e) => {
-        let target = e.target
-        let rect = target.getClientRects()[0]
-        let previous
+        let rect = e.target.getBoundingClientRect()
 
-        hover.style.height = target.offsetHeight + 'px'
-        hover.style.width = target.offsetWidth + 'px'
-        
-        // hover.style.top = `${Math.round(rect.top) + Number(styles.padding.split('px')[0])}px`
-        // hover.style.left = target.offsetLeft + 'px'
-
-        let start = performance.now()
-        let duration = 500
-
-        requestAnimationFrame(function animate(time){
-            let diff = time - start
-            let fraction = diff/duration
-            let hoverTop = Number(hover.style.top.split('px')[0])
-            let hoverLeft = Number(hover.style.left.split('px')[0])
-
-            hover.style.top = (hoverTop + (((target.offsetTop + target.offsetParent.offsetTop)- hoverTop) * fraction)) + 'px';
-
-            hover.style.left = (hoverLeft + ((Number(target.offsetLeft - hoverLeft)) * fraction)) + 'px'
-            
-            if(diff < 1000){
-                requestAnimationFrame(animate)
-            }
-        })
-        console.log(target.offsetTop + target.offsetParent.offsetTop)
+        hover.style.width = `${rect.width}px`
+        hover.style.height = `${rect.height}px`
+        hover.style.transform = `translate(${Math.round(e.target.offsetLeft)}px, ${(e.target.offsetTop - 50 + e.target.offsetParent.offsetTop)}px)`
+        // console.log(rect)
     })
+
+
+
+    // value.addEventListener('mouseover', (e) => {
+    //     let target = e.target
+    //     let rect = target.getClientRects()[0]
+    //     let previous
+
+    //     hover.style.height = target.offsetHeight + 'px'
+    //     hover.style.width = target.offsetWidth + 'px'
+        
+    //     // hover.style.top = `${Math.round(rect.top) + Number(styles.padding.split('px')[0])}px`
+    //     // hover.style.left = target.offsetLeft + 'px'
+
+    //     let start = performance.now()
+    //     let duration = 500
+
+    //     requestAnimationFrame(function animate(time){
+    //         let diff = time - start
+    //         let fraction = diff/duration
+    //         let hoverTop = Number(hover.style.top.split('px')[0])
+    //         let hoverLeft = Number(hover.style.left.split('px')[0])
+
+    //         hover.style.top = (hoverTop + (((target.offsetTop + target.offsetParent.offsetTop)- hoverTop) * fraction)) + 'px';
+
+    //         hover.style.left = (hoverLeft + ((Number(target.offsetLeft - hoverLeft)) * fraction)) + 'px'
+            
+    //         if(diff < 1000){
+    //             requestAnimationFrame(animate)
+    //         }
+    //     })
+    //     console.log(target.offsetTop + target.offsetParent.offsetTop)
+    // })
 })
